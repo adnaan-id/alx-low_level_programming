c0edd2afb06619d6da73807be92e3b01ce32d60b
adnaan-id <adnandikko@gmail.com>
2023-02-23 09:29:54 -0800

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main - test function for positive or negative
+ * main - test function ft prints if integer is or positive or negative
  * Return: 0
  */
 
+
 int main(void);
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
